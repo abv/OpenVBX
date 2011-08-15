b7f7f24f74a74e8f35704bc4839e71b9fe8cd40e
Adam Varga <me@adamvarga.com>
2011-08-15 13:46:22 -0400

    Keeping update scripts compatible with prefixed database tables

--- updates/51.sql
+++ updates/51.sql
@@ -1,2 +1,2 @@
-UPDATE `settings` SET `value` = '1.0.2' WHERE `name` = 'version';
-UPDATE `settings` SET `value` = 51 WHERE `name` = 'schema-version';
+UPDATE `{PRE}settings` SET `value` = '1.0.2' WHERE `name` = 'version';
+UPDATE `{PRE}settings` SET `value` = 51 WHERE `name` = 'schema-version';
--- updates/52.sql
+++ updates/52.sql
@@ -1,2 +1,2 @@
-UPDATE `settings` SET `value` = '1.0.3' WHERE `name` = 'version';
-UPDATE `settings` SET `value` = 52 WHERE `name` = 'schema-version';
+UPDATE `{PRE}settings` SET `value` = '1.0.3' WHERE `name` = 'version';
+UPDATE `{PRE}settings` SET `value` = 52 WHERE `name` = 'schema-version';
